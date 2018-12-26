0e9efe0838779322980e118c74a7228857d50c38
Ailson <ailson_freire@outlook.com>
2018-12-26 15:48:04 -0300

    Apk gerada

--- android/app/src/main/java/com/blueticket/MainApplication.java
+++ android/app/src/main/java/com/blueticket/MainApplication.java
@@ -3,7 +3,6 @@ package com.blueticket;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.react.rnspinkit.RNSpinkitPackage;                                                                                                                                                            
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSpinkitPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage()
       );
